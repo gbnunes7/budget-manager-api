43660ca0c43ea5d7b5b7b54680ca61f7a133fdaf
Gabriel <bielrj170@gmail.com>
2025-03-11 22:47:12 -0300

    feat: create a transaction entity

---
+++ src/domain/budget-manager/enterprise/entities/transaction.ts
@@ -0,0 +1,64 @@
+import { Entity } from '../../../../core/entitites/entity';
+import type { UniqueEntityId } from '../../../../core/entitites/unique-entity-id';
+
+interface TransactionProps {
+  categoryId: UniqueEntityId;
+  amount: number;
+  description: string;
+  date: Date;
+  balanceId: UniqueEntityId;
+  type: 'EXPENSE' | 'INCOME';
+  userId: UniqueEntityId;
+}
+
+class Transaction extends Entity<TransactionProps> {
+  get categoryId(): UniqueEntityId {
+    return this.props.categoryId;
+  }
+
+  get amount(): number {
+    return this.props.amount;
+  }
+
+  get description(): string {
+    return this.props.description;
+  }
+
+  get date(): Date {
+    return this.props.date;
+  }
+
+  get type(): 'EXPENSE' | 'INCOME' {
+    return this.props.type;
+  }
+
+  get userId(): UniqueEntityId {
+    return this.props.userId;
+  }
+
+  set categoryId(categoryId: UniqueEntityId) {
+    this.props.categoryId = categoryId;
+  }
+
+  set amount(amount: number) {
+    this.props.amount = amount;
+  }
+
+  set description(description: string) {
+    this.props.description = description;
+  }
+
+  set type(type: 'EXPENSE' | 'INCOME') {
+    this.props.type = type;
+  }
+
+  private constructor(props: TransactionProps, id?: string) {
+    super(props, id);
+  }
+
+  public static create(props: TransactionProps, id?: string): Transaction {
+    return new Transaction(props, id);
+  }
+}
+
+export { Transaction };

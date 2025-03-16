6c8c1afdae17733e54c8d36fbc3cdde9a3f92e34
Gabriel <bielrj170@gmail.com>
2025-03-16 01:36:22 -0300

    feat: insert new methods in category contract

--- src/domain/budget-manager/application/repositories/category-repository.ts
+++ src/domain/budget-manager/application/repositories/category-repository.ts
@@ -2,4 +2,6 @@ import type { Category } from '../../enterprise/entities/category';
 
 export interface ICategoryRepository {
   create(category: Category): Promise<void>;
+  delete(category: Category): Promise<void>;
+  findById(id: string): Promise<Category | null>;
 }

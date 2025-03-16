c0c019d12f5666ebcf6e20f6a655a10bda8cd51a
Gabriel <bielrj170@gmail.com>
2025-03-16 01:46:05 -0300

    feat: create an unit test for use case

---
+++ src/domain/budget-manager/application/use-cases/delete-transaction-use-case.spec.ts
@@ -0,0 +1,42 @@
+import { expect, it, describe, beforeEach } from 'vitest';
+import { InMemoryTransactionRepository } from '../../../../../test/repositories/in-memory-transaction-repository';
+import { UniqueEntityId } from '../../../../core/entitites/unique-entity-id';
+import { DeleteTransactionUseCases } from './delete-transaction-use-case';
+import { Transaction } from '../../enterprise/entities/transaction';
+
+let transactionRepository: InMemoryTransactionRepository;
+let sut: DeleteTransactionUseCases;
+
+describe('DeleteTransactionUseCases', () => {
+  beforeEach(() => {
+    transactionRepository = new InMemoryTransactionRepository();
+    sut = new DeleteTransactionUseCases(transactionRepository);
+  });
+
+  it('should be able to delete a transaction', async () => {
+    const transaction = Transaction.create({
+      amount: 100,
+      type: 'EXPENSE',
+      balanceId: new UniqueEntityId('1'),
+      categoryId: new UniqueEntityId('1'),
+      date: new Date(),
+      description: 'Test transaction',
+      userId: new UniqueEntityId('1'),
+    });
+
+    await transactionRepository.create(transaction);
+
+    await sut.execute(transaction.id.toString());
+
+    const foundTransaction = await transactionRepository.findById(
+      transaction.id.toString(),
+    );
+    expect(foundTransaction).toBeNull();
+  });
+
+  it('should throw an error if the transaction does not exist', async () => {
+    await expect(sut.execute('1')).rejects.toThrowError(
+      'Transaction not found',
+    );
+  });
+});

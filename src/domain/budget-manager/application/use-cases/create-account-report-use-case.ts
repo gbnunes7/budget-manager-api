fe2c1f8099ecb1d574e9b4178c990dbcd4fc322c
Gabriel <bielrj170@gmail.com>
2025-03-16 23:35:35 -0300

    feat: create an account report request use case

---
+++ src/domain/budget-manager/application/use-cases/create-account-report-use-case.ts
@@ -0,0 +1,36 @@
+import { UniqueEntityId } from '../../../../core/entitites/unique-entity-id';
+import { AccountReport } from '../../enterprise/entities/account-report';
+import type { IAccountReportRepository } from '../repositories/account-report-repository';
+
+interface GenerateAccountReportInput {
+  userId: string;
+  totalBalance: number;
+  totalIncome: number;
+  totalExpense: number;
+  startDate: Date;
+  endDate: Date;
+}
+
+class GenerateAccountReportUseCase {
+  constructor(private accountReportRepository: IAccountReportRepository) {}
+
+  async execute(input: GenerateAccountReportInput): Promise<void> {
+    console.log(
+      '[DEBUG] Gerando relatório financeiro para o usuário:',
+      input.userId,
+    );
+
+    const report = AccountReport.generateReport(
+      new UniqueEntityId(input.userId),
+      input.totalBalance,
+      input.totalIncome,
+      input.totalExpense,
+      input.startDate,
+      input.endDate,
+    );
+
+    await this.accountReportRepository.create(report);
+  }
+}
+
+export { GenerateAccountReportUseCase };
